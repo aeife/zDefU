42df28e9682f7e4806032d87dc111be041e97683
Andre Eife <ande@spreadshirt.net>
2014-06-05 16:49:06 +0200

    spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public float spawnTime = 5f;
+	public float spawnDelay = 3f;
+	public GameObject zombie;
+
+	// Use this for initialization
+	void Start () {
+		// TODO: use for soldier shoot
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+
+	void Spawn () {
+		Instantiate (zombie, transform.position, transform.rotation);
+	}
+}
