b4b2389178623b14fb18dc37c191891184a70f1b
Andre Eife <ande@spreadshirt.net>
2014-06-07 17:24:06 +0200

    zombie prefab

---
+++ zDefU3D/Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class Movement : MonoBehaviour {
+	Seeker seeker;
+	public float moveSpeed = 10.0f;
+	float rotationSpeed = 50.0f;
+	Path path;
+	int currentWaypoint = 0;
+	float nextWaypointDistance = 3;
+	
+	// Use this for initialization
+	void Start () {
+		seeker = GetComponent<Seeker>();
+	}
+
+	public void moveTo (Vector3 targetPosition) {
+		seeker.StartPath (transform.position, targetPosition, OnPathComplete);
+	}
+	
+	public void OnPathComplete (Path p) {
+		Debug.Log ("Yey, we got a path back. Did it have an error? "+p.error);
+		if (!p.error) {
+			path = p;
+			//Reset the waypoint counter
+			currentWaypoint = 0;
+		}
+	}
+	
+	void FixedUpdate () {
+		if (path == null) {
+			//We have no path to move after yet
+			return;
+		}
+		
+		if (currentWaypoint >= path.vectorPath.Count) {
+			//			Debug.Log ("End Of Path Reached");
+			return;
+		}
+		
+		//Direction to the next waypoint
+		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized;
+		dir *= moveSpeed * Time.fixedDeltaTime;
+		// move to direction
+		rigidbody.MovePosition(rigidbody.position + dir);
+		
+		//Check if we are close enough to the next waypoint
+		//If we are, proceed to follow the next waypoint
+		if (Vector3.Distance (transform.position,path.vectorPath[currentWaypoint]) < nextWaypointDistance) {
+			currentWaypoint++;
+			return;
+		}
+	}
+}
---
+++ zDefU3D/Assets/Scripts/ZombieBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class ZombieBehaviour : MonoBehaviour {
+	GameObject[] soldiers;
+	Movement moveScript;
+	// time in seconds to look again for nearest soldier to attack
+	public float refreshRate = 0.1f;
+	float currentRefreshCountdown;
+	Vector3 lastTargetPosition;
+
+	// Use this for initialization
+	void Start () {
+		moveScript = GetComponent<Movement>();
+		currentRefreshCountdown = refreshRate;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (currentRefreshCountdown <= 0) {
+			currentRefreshCountdown = refreshRate;
+			attack (getNearestSoldier ());
+		} else {
+			currentRefreshCountdown  -= Time.deltaTime;
+		}
+	}
+
+	void attack (GameObject target){
+		if (Vector3.Distance (lastTargetPosition, target.transform.position) < 1) {
+			return;
+		}
+		if (target) {
+			lastTargetPosition = target.transform.position;
+			moveScript.moveTo(target.transform.position);
+		}
+	}
+
+	GameObject getNearestSoldier () {
+		float minDistance = float.MaxValue;
+		soldiers = GameObject.FindGameObjectsWithTag ("Soldier");
+		GameObject nearestSoldier = null;
+		foreach (GameObject soldier in soldiers) {
+			float distance = Vector3.Distance(transform.position, soldier.transform.position);
+			if (distance < minDistance) {
+				minDistance = distance;
+				nearestSoldier = soldier;
+			}
+		}
+		
+		return nearestSoldier;
+	}
+}
